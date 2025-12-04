e8801ba4373196538e772444d45168dc81519fe0
Nova <aelithron@gmail.com>
2025-12-04 16:20:46 +0000

    Upgrade NextJS, improve workflows

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/edit') && (req.cookies.get('auth')?.value !== process.env.WEBEDITOR_PASSWORD)) {
     return NextResponse.redirect(new URL('/editor-login', req.url));
   }
